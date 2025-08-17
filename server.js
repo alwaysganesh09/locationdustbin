3da5d203f4e93c9725a52419b778a7f33ec4e4ce
alwaysganesh09 <neelig552@gmail.com>
2025-08-17 17:41:17 +0530

    addedpublic folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000; // Vercel ignores this PORT
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
 let db;
